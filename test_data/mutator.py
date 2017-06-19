ef5e62b0156d5a733761d34cac2f34aeeccf9413
Charles Soeder <Charles.Soeder@gmail.com>
2017-06-19 13:39:14 -0400

    cleaned up the mutator script; specify mut_rate from command line

--- test_data/mutator.py
+++ test_data/mutator.py
@@ -4,41 +4,56 @@ from Bio.SeqRecord import SeqRecord
 from random import randint, choice
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument("reference_genome", help="reference genome to be mutated")
+parser.add_argument("custom_genome", help="output genome, with SNPs added")
+
+parser.add_argument("--mutation_rate", help="SNP rate, per bp; default 1%", type=float )
+
+args = parser.parse_args()
+fasta_in = args.reference_genome
+fasta_out = args.custom_genome
+if args.mutation_rate == None:
+	mut_rate=0.01 # 	SNP frequency, per bp
+else:
+	mut_rate = args.mutation_rate
+
+
+
 #samtools faidx /proj/cdjones_lab/Genomics_Data_Commons/genomes/drosophila_melanogaster/dm6.fa chr2L > dm6.chr2L.fa
 
-mut_rate=0.001 # 	SNP frequency, per bp
+chroms_out = []
 
-std_chrom = SeqIO.parse("dm6.chr2L.fa", "fasta").next().seq.tostring()
+for std_chrom in SeqIO.write(fasta_in, "fasta"):
 
-snp_sites = []
+	std_chrom = std_chrom.seq.tostring()
 
-for i in range(1, int(len(std_chrom)*mut_rate)): #	calculate number of sites to perturb
-	snp_sites.append(randint(1, len(std_chrom)))
+	snp_sites = []
 
-snp_sites = list(set(snp_sites))
-snp_sites.sort()
-snp_sites.reverse()
+	for i in range(1, int(len(std_chrom)*mut_rate)): #	calculate number of sites to perturb
+		snp_sites.append(randint(1, len(std_chrom)))
 
-nu_start=0
-nu_chrom=''
-while len(snp_sites) > 0:
-	old_start = nu_start
-	nu_start = snp_sites.pop()
-	nukes = ["A","T","C","G"]
-	try:
-		nukes.remove(std_chrom[nu_start].upper())
-	except ValueError:
-		#got one of these; if the nucleotide in the standard chromosome isn't in the replacement list
-		#eg, N
-		#don't worry about it
-		pass
-	nu_chrom = "%s%s%s" % tuple([nu_chrom, std_chrom[old_start:nu_start-1], choice(nukes)])
-nu_chrom = "%s%s" % tuple([nu_chrom, std_chrom[nu_start:]])
+	snp_sites = list(set(snp_sites))
+	snp_sites.sort()
+	snp_sites.reverse()
+
+	nu_start=0
+	nu_chrom=''
+	while len(snp_sites) > 0:
+		old_start = nu_start
+		nu_start = snp_sites.pop()
+		nukes = ["A","T","C","G"]
+		try:
+			nukes.remove(std_chrom[nu_start].upper())
+		except ValueError:
+			#got one of these; if the nucleotide in the standard chromosome isn't in the replacement list
+			#eg, N
+			#don't worry about it
+			pass
+		nu_chrom = "%s%s%s" % tuple([nu_chrom, std_chrom[old_start:nu_start-1], choice(nukes)])
+	nu_chrom = "%s%s" % tuple([nu_chrom, std_chrom[nu_start:]])
+
+	chroms_out.append(SeqRecord(Seq(nu_chrom), id="chr2L", name="chr2L"))
 
 
-record=SeqRecord(Seq(nu_chrom),
-                   id="chr2L", name="chr2L")
-print len(nu_chrom)
-print len(std_chrom)
-
-SeqIO.write(record, "parent.fasta", "fasta")
+SeqIO.write(chroms_out, fasta_out, "fasta")
