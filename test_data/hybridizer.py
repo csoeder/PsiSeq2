a1bb4e36dcee1347c047fa961f263c12a930cf24
Charles Soeder <Charles.Soeder@gmail.com>
2017-06-19 13:44:33 -0400

    cleaned up hybridizer; command-line options. Still needs multichromosome functionality

--- test_data/hybridizer.py
+++ test_data/hybridizer.py
@@ -9,10 +9,13 @@ parser.add_argument("parent1", help="first parent")
 parser.add_argument("parent2", help="second parent")
 parser.add_argument("hybrid_fasta", help="hybrid FASTA out")
 parser.add_argument("collage", help="BED file of hybrid ancestry out")
+parser.add_argument("--introgression_number", help="number of introgression sites, total", type=int, default=10 )
+parser.add_argument("--introgression_size", help="size of introgression sites, in bp", type=int, default=5000 )
+
 args = parser.parse_args()
 
-introg_num = 10
-introg_size = 5000
+introg_num = args.introgression_number
+introg_size = args.introgression_size
 introg_points = []
 
 
